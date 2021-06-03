3a0530250a814ab95531ed2d5d05303575de912c
Mave-rick-dev <sangam.ale.sa63@gmail.com>
2021-06-03 21:59:23 +0545

    Return list of varying banknotes equal to the withdraw amount.

--- src/main/java/com/progressoft/induction/atm/ATM.java
+++ src/main/java/com/progressoft/induction/atm/ATM.java
@@ -3,7 +3,7 @@ package com.progressoft.induction.atm;
 import java.math.BigDecimal;
 import java.util.List;
 
-public interface ATM {
+public interface ATM  {
 
     List<Banknote> withdraw(String accountNumber, BigDecimal amount);
 }
